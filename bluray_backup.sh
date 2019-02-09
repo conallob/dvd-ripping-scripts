3015e2cc3fe655ff3d15b9a29f5517788f9e2bb5
Conall O'Brien <conall@conall.net>
2019-02-09 14:14:10 +0000

    ~ Made a few adjustments following emperical testing

--- bluray_backup.sh
+++ bluray_backup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+TITLE=$1
+OUTPUT_DIR="/Users/Shared/DVDRips"
+
 # Use makemkvcon to decrypt the Blu Ray disk
-makemkvcon backup --decrypt --cache=16 --noscan \
-  -r --progress=-same disc:1 \
-  $1
+makemkvcon backup --decrypt --cache=16 --noscan -r --progress=-same disc:1 \
+  "${OUTPUT_DIR}/${TITLE}/"
 
 # Eject the disk
 hdiutil eject /dev/disk1
